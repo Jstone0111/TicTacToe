27ece2e4b791ee49834d687d0c270df3a8e3ff24
Jacob Stone <Jacobstone0111@yahoo.com>
2021-04-05 18:43:47 -0500

    Made it to where it you can not click the same box twice.

--- app.js
+++ app.js
@@ -1,17 +1,39 @@
 let cells = document.querySelectorAll('.row > div');
+let currentPlayer;
+let shape = 1;
+
 
 for (let i = 0; i < cells.length; i++){
+    cells[i].innerText == '';
     cells[i].addEventListener('click', cellClicked);
 }
 
-let shape = 1;
-function cellClicked(){
+
+function cellClicked(event){ //Handles the click and player swap.
+    if (event.target.textContent !== '') return; //If cell is not empty stop
 
     if (shape == 1) {
-        event.target.textContent = 'X'
+        currentPlayer = 'X';
+        event.target.textContent = currentPlayer;
         shape = 0;
     } else {
-        event.target.textContent = 'O'
+        currentPlayer = 'O';
+        event.target.textContent = currentPlayer;
         shape = 1;
     }
-}  
+ 
+    winningCondition();
+}
+
+function winningCondition(){
+    if (cells[0].textContent == cells[1].textContent &&
+        cells[1].textContent == cells[2].textContent &&
+        cells[0].textContent == 'X'){
+            console.log("X Won!");
+        }
+
+    console.log(cells[0].classList);
+    console.log(cells[1].textContent);
+    console.log(cells[2].textContent);
+}
+
