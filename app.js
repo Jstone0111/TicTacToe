801cd2d8e742333edf3c62c7205976b9377263b6
Jacob Stone <Jacobstone0111@yahoo.com>
2021-04-05 19:13:23 -0500

    Removed the 'Shape' variable and added the checkWin() function.

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 let cells = document.querySelectorAll('.row > div');
-let currentPlayer;
-let shape = 1;
+let currentPlayer = 'X';
 
 
 for (let i = 0; i < cells.length; i++){
@@ -12,28 +11,37 @@ for (let i = 0; i < cells.length; i++){
 function cellClicked(event){ //Handles the click and player swap.
     if (event.target.textContent !== '') return; //If cell is not empty stop
 
-    if (shape == 1) {
-        currentPlayer = 'X';
+    if (currentPlayer === 'X') {
         event.target.textContent = currentPlayer;
-        shape = 0;
+        currentPlayer = 'O';
     } else {
         currentPlayer = 'O';
         event.target.textContent = currentPlayer;
-        shape = 1;
+        currentPlayer = 'X';
     }
  
-    winningCondition();
+    checkWin();
 }
 
-function winningCondition(){
-    if (cells[0].textContent == cells[1].textContent &&
-        cells[1].textContent == cells[2].textContent &&
-        cells[0].textContent == 'X'){
-            console.log("X Won!");
+function checkWin(){
+    if (
+        (cells[0].textContent !== '' && cells[0].textContent === cells[1].textContent && cells[2].textContent) ||
+        (cells[3].textContent !== '' && cells[3].textContent === cells[4].textContent && cells[5].textContent) ||
+        (cells[6].textContent !== '' && cells[6].textContent === cells[7].textContent && cells[8].textContent) ||
+        
+        (cells[0].textContent !== '' && cells[0].textContent === cells[3].textContent && cells[6].textContent) ||
+        (cells[1].textContent !== '' && cells[1].textContent === cells[4].textContent && cells[7].textContent) ||
+        (cells[2].textContent !== '' && cells[2].textContent === cells[5].textContent && cells[8].textContent) ||
+        
+        (cells[0].textContent !== '' && cells[0].textContent === cells[4].textContent && cells[8].textContent) ||
+        (cells[2].textContent !== '' && cells[2].textContent === cells[4].textContent && cells[6].textContent)
+    )  {
+        if (currentPlayer === 'X'){
+            alert('O Wins!');
+        } else{
+                alert('X Wins!');
         }
+    }
+}   
 
-    console.log(cells[0].classList);
-    console.log(cells[1].textContent);
-    console.log(cells[2].textContent);
-}
 
